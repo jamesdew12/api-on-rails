6abf885c6f7289bf78589f8ae80723016dc33061
James de Winton <james.dewinton@gmail.com>
2019-03-07 12:43:24 +0000

    form for new created

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,5 +7,17 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+  def new
+    @recipe = Artist.new
+
+  end
+  def destroy
+    artist = Artist.find(params[:id])
+    artist.destroy
+    redirect_to artists_path, notice: "Deleted Artist: #{artist.name}"
+
+  end
+
+
 
 end
