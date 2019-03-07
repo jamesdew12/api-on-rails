14bfde950bc942e482db0e3a9b5fe81914646dc7
James de Winton <james.dewinton@gmail.com>
2019-03-07 13:46:23 +0000

    add users

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,12 +9,29 @@ class ArtistsController < ApplicationController
   end
   def new
     @recipe = Artist.new
-
   end
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to artist_path(@artist), notice: "artist created"
+    else
+      @errors = @artist.errors.full_messages
+      render :new
+    end
+  end
+
   def destroy
     artist = Artist.find(params[:id])
     artist.destroy
     redirect_to artists_path, notice: "Deleted Artist: #{artist.name}"
+
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :description)
 
   end
 
